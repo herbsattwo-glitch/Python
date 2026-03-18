fa3b02060a3a9693a35eafe9d42b5cedd01e8f63
herbsattwo-glitch <herbsattwo@gmail.com>
2026-03-18 06:59:51 +0200

    Delete Python directory

--- Python/Chapter_1/hello _world.py
+++
@@ -1,3 +0,0 @@
-print("Hello Motto!")
-
-print("Bankai!")
--- Python/Chapter_9/test_ in_shell.py
+++
@@ -1,8 +0,0 @@
-from random import randint
-randint(1, 6)
-
-from random import choice
-
-players = ['charles', 'martina', 'michael', 'florence', 'eli']
-first_up = choice(players)
-first_up
